8f48ff7ff95dafc0f50cb7b430efe7e1faf987ed
Muhammad Surya Jayadiprana <msurya672@gmail.com>
2024-11-22 01:43:18 +0700

    Google Scholar Author Scrap 🐐
    Google Scholar Author Scrap 🐐

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,65 +29,59 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-12 col-12">
-          <div class="col-lg-12 col-md-12 col-12">
-            <div class="card shadow-sm">
-              <div class="card-header">
-                Data Artikel
-                {#if params}
-                <button class="btn btn-primary float-end" onclick={() => exportToExcel(articles)}>Download Excel</button>
-              {/if}
-              </div>
-              <div class="card-body">
-                <table class="table table-striped table-bordered">
-                  <thead>
-                    <tr>
-                      <th>#</th>
-                      <th>Judul</th>
-                      <th>Tahun</th>
-                      <th>Link Publikasi</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {#each articles as item,i}
-                       <tr>
-                        <td>{i + 1}</td>
-                        <td>{item.title}</td>
-                        <td>{item.year}</td>
-                        <td>
-                          <a href={item.url}>{item.title}</a>
-                        </td>
-                       </tr>
-                    {:else}
-                       <tr>
-                        <td colspan="4" class="text-center">Data artikel tidak di temukan</td>
-                       </tr>
-                    {/each}
-                  </tbody>
-                </table>
-              </div>
+          <div class="card shadow-sm">
+            <div class="card-header">
+              Data Artikel
+              {#if params}
+              <button class="btn btn-primary float-end" onclick={() => exportToExcel(articles)}>Download Excel</button>
+            {/if}
+            </div>
+            <div class="card-body">
+              <table class="table table-striped table-bordered">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Judul</th>
+                    <th>Tahun</th>
+                    <th>Link Publikasi</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {#each articles as item,i}
+                     <tr>
+                      <td>{i + 1}</td>
+                      <td>{item.title}</td>
+                      <td>{item.year}</td>
+                      <td>
+                        <a href={item.url}>{item.title}</a>
+                      </td>
+                     </tr>
+                  {:else}
+                     <tr>
+                      <td colspan="4" class="text-center">Data artikel tidak di temukan</td>
+                     </tr>
+                  {/each}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-        <div class="col-lg-4 col-md-12 col-12">
-          <div class="row">
-            <div class="col-lg-12 col-md-12 col-12">
-              <div class="card shadow-sm">
-                <div class="card-header">
-                  Cari Author
+        <div class="col-lg-4 col-md-12 col-12 order-lg-last order-first">
+          <div class="card shadow-sm mb-3">
+            <div class="card-header">
+              Cari Author
+            </div>
+            <div class="card-body">
+              <div class="row align-items-center">
+                <div class="col-lg-6 col-md-12 col-12">
+                  <div class="form-floating mb-3">
+                    <input type="text" class="form-control w-100" id="id" placeholder="User ID" bind:value={id} required>
+                    <label for="id">User ID</label>
+                  </div>
                 </div>
-                <div class="card-body">
-                  <div class="row align-items-center">
-                    <div class="col-lg-6 col-md-12 col-12">
-                      <div class="form-floating mb-3">
-                        <input type="text" class="form-control w-100" id="id" placeholder="User ID" bind:value={id} required>
-                        <label for="id">User ID</label>
-                      </div>
-                    </div>
-                    <div class="col-lg-6 col-md-12 col-12">
-                      <div class="mb-3">
-                        <button class="btn btn-primary w-100" type="submit" onclick={() => goto(`/?id=${id}`)}>Kirim</button>
-                      </div>
-                    </div>
+                <div class="col-lg-6 col-md-12 col-12">
+                  <div class="mb-3">
+                    <button class="btn btn-primary w-100" type="submit" onclick={() => goto(`/?id=${id}`)}>Kirim</button>
                   </div>
                 </div>
               </div>
